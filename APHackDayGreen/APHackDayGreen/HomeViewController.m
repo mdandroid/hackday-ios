749d7797896e9784c5ee7e94b06ef0159d84a3fa
Maria La Porta <mlaporta@odecee.com.au>
2015-05-26 18:32:01 +1000

    Laying out the home page - messages section

--- APHackDayGreen/APHackDayGreen/HomeViewController.m
+++ APHackDayGreen/APHackDayGreen/HomeViewController.m
@@ -9,6 +9,8 @@
 #import "HomeViewController.h"
 #import "DetailViewController.h"
 #import "TTTAttributedLabel.h"
+#import "UIColor+Extensions.h"
+
 
 @interface HomeViewController ()
 
@@ -19,163 +21,157 @@
 @property (nonatomic, strong) UIButton *okButton;
 @property (nonatomic, strong) UIButton *waitMoreButton;
 @property (nonatomic, strong) UIView *buttonContainer;
+@property (nonatomic, strong) UIImageView *vanImage;
+@property (nonatomic, strong) UIImageView *houseImage;
 
 @end
 
 @implementation HomeViewController
 
-//- (void)loadView {
-//    [super loadView];
-//
-//    self.label = [[UILabel alloc]initWithFrame:CGRectZero];
-//    
-//    self.view.backgroundColor = [UIColor whiteColor];
-//    
-//    
-//    NSString *message = @"Your parcel from ASOS is on board for delivery!";
-//    NSString *message2 = @"Our driver will be at:";
-//    NSString *message3 = @"123 Smith Street, Fitzroy VIC\n today between 12:00pm - 3:00pm";
-//    
-//   // self.label.backgroundColor = [UIColor greenColor];
-//    self.label.textColor = [UIColor blackColor];
-//    self.label.translatesAutoresizingMaskIntoConstraints = NO;
-//    //self.label.textAlignment = NSTextAlignmentCenter;
-//    self.label.numberOfLines = 0;
-//    self.label.lineBreakMode = NSLineBreakByWordWrapping;
-//    self.label.text = message;
-//    
-//    
-//    
-//   // NSRange rangeOfSender = [message rangeOfString:@"ASOS"];
-//   // NSRange rangeOfAddress = [message rangeOfString:@"Home"];
-//    
-//    
-//    UIFont *mainFont = [UIFont fontWithName:@"APLetter-Regular" size:14.f];;
-//    //NSMutableAttributedString *aString = [[NSMutableAttributedString alloc]initWithString:message];
-//    
-//   // [aString addAttribute:NSFontAttributeName value:mainFont range:NSMakeRange(0, aString.length)];
-// //   [aString addAttribute:NSFontAttributeName value:[UIFont boldHelveticaFontOfSize:14.f] range:rangeOfPin];
-////    aString addAttribute:NSUnderlineStyleAttributeName value:<#(id)#> range:<#(NSRange)#>
-////    [aString addAttribute:NSForegroundColorAttributeName  value:[UIColor colorWithHexValue:APColourTextFieldBorder] range:NSMakeRange(0, aString.length)];
-////    self.helpInstructionsLabel.attributedText = aString;
-//  //  self.label.attributedText = aString;
-//    
-//    
-// //   self.label.attributedText = @"Hi Sam!\n\n Your parcel from ASOS will be delivered to your Home address today between 12pm - 3pm.";
-//    
-//    // Label2
-//    self.label2 = [[UILabel alloc] initWithFrame:CGRectZero];
-//    self.label2.translatesAutoresizingMaskIntoConstraints = NO;
-//    self.label2.font = [UIFont fontWithName:@"APLetter-Light" size:14.f];
-//    self.label2.textColor = [UIColor blackColor];
-//    self.label2.text = message2;
-//    self.label2.numberOfLines = 0;
-//    self.label2.lineBreakMode = NSLineBreakByWordWrapping;
-//    
-//    
-//    // Label3
-//    self.label3 = [[UILabel alloc] initWithFrame:CGRectZero];
-//    self.label3.translatesAutoresizingMaskIntoConstraints = NO;
-//    self.label3.font = [UIFont fontWithName:@"APLetter-Light" size:14.f];
-//    self.label3.textColor = [UIColor blackColor];
-//    self.label3.text = message;
-//    self.label3.numberOfLines = 0;
-//    self.label3.lineBreakMode = NSLineBreakByWordWrapping;
-//    
-//    // MEssage container
-//    self.messageContainer = [[UIView alloc] initWithFrame:CGRectZero];
-//    self.messageContainer.backgroundColor = [UIColor lightGrayColor];
-//    self.messageContainer.translatesAutoresizingMaskIntoConstraints = NO;
-//    
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"Confirm Delivery";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    NSString *message = @"Your parcel from ASOS is on board for delivery!";
+    NSString *message2 = @"Our driver will be at:";
+    NSString *message3 = @"123 Smith Street, Fitzroy VIC today between 12:00pm - 3:00pm";
+    
+    self.label = [[UILabel alloc]initWithFrame:CGRectZero];
+    self.label.backgroundColor = [UIColor colorWithHexValue:@"F3F1EE"];
+    self.label.font = [UIFont fontWithName:@"APLetter-Light" size:16.f];
+    self.label.textColor = [UIColor colorWithHexValue:@"382F2D"];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.numberOfLines = 0;
+    self.label.lineBreakMode = NSLineBreakByWordWrapping;
+    self.label.text = message;
+
+
+    // Label2
+    self.label2 = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.label2.font = [UIFont fontWithName:@"APLetter-Light" size:16.f];
+    self.label2.textColor = [UIColor colorWithHexValue:@"807370"];
+    self.label2.backgroundColor = [UIColor colorWithHexValue:@"F3F1EE"];
+    self.label2.textAlignment = NSTextAlignmentCenter;
+    self.label2.text = message2;
+    self.label2.numberOfLines = 0;
+    self.label2.textAlignment = NSTextAlignmentCenter;
+    self.label2.lineBreakMode = NSLineBreakByWordWrapping;
+
+    
+    // Label3
+    self.label3 = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.label3.backgroundColor = [UIColor colorWithHexValue:@"F3F1EE"];
+    self.label3.font = [UIFont fontWithName:@"APLetter-Light" size:19.f];
+    self.label3.textColor = [UIColor colorWithHexValue:@"382F2D"];
+    self.label3.text = message3;
+    self.label3.numberOfLines = 0;
+    self.label3.textAlignment = NSTextAlignmentCenter;
+    self.label3.lineBreakMode = NSLineBreakByWordWrapping;
+    
+    // Images
+    self.vanImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img-van"]];
+    self.houseImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"img-house"]];
+    
+    
+   // NSRange rangeOfSender = [message rangeOfString:@"ASOS"];
+   // NSRange rangeOfAddress = [message rangeOfString:@"Home"];
+
+
+    UIFont *mainFont = [UIFont fontWithName:@"APLetter-Regular" size:14.f];;
+    //NSMutableAttributedString *aString = [[NSMutableAttributedString alloc]initWithString:message];
+
+   // [aString addAttribute:NSFontAttributeName value:mainFont range:NSMakeRange(0, aString.length)];
+ //   [aString addAttribute:NSFontAttributeName value:[UIFont boldHelveticaFontOfSize:14.f] range:rangeOfPin];
+//    aString addAttribute:NSUnderlineStyleAttributeName value:<#(id)#> range:<#(NSRange)#>
+//    [aString addAttribute:NSForegroundColorAttributeName  value:[UIColor colorWithHexValue:APColourTextFieldBorder] range:NSMakeRange(0, aString.length)];
+//    self.helpInstructionsLabel.attributedText = aString;
+  //  self.label.attributedText = aString;
+
+
+ //   self.label.attributedText = @"Hi Sam!\n\n Your parcel from ASOS will be delivered to your Home address today between 12pm - 3pm.";
+
+
+
+
+
+
+    // MEssage container
+    self.messageContainer = [[UIView alloc] initWithFrame:CGRectZero];
+    self.messageContainer.backgroundColor = [UIColor colorWithHexValue:@"F3F1EE"];
+    
+
 //    // Button Container
 //    self.buttonContainer =[ [UIView alloc] initWithFrame:CGRectZero];
 //    self.buttonContainer.backgroundColor = [UIColor whiteColor
 //                                            ];
 //    self.buttonContainer.translatesAutoresizingMaskIntoConstraints = NO;
-//    
+//
 //    // Buttons
 //    self.okButton = [UIButton buttonWithType:UIButtonTypeSystem];
 //    [self.okButton setTitle:@"OK, I'm home" forState:UIControlStateNormal];
 //    [self.view addSubview:self.okButton];
 //    [self.okButton addTarget:self action:@selector(nextPage:) forControlEvents:UIControlEventTouchUpInside];
 //    self.okButton.translatesAutoresizingMaskIntoConstraints = NO;
-//    
+//
 //    self.waitMoreButton = [UIButton buttonWithType:UIButtonTypeSystem];
 //    [self.waitMoreButton setTitle:@"Wait! More Options" forState:UIControlStateNormal];
 //    [self.view addSubview:self.waitMoreButton];
 //    [self.waitMoreButton addTarget:self action:@selector(confirmPage:) forControlEvents:UIControlEventTouchUpInside];
 //    self.waitMoreButton.translatesAutoresizingMaskIntoConstraints = NO;
 //
-//    
-//    
-//    [self.messageContainer addSubview:self.label];
-//    [self.messageContainer addSubview:self.label2];
-//    [self.messageContainer addSubview:self.label3];
-//    [self.view addSubview:self.messageContainer];
-//    
+//
+//
+    [self.messageContainer addSubview:self.label];
+    [self.messageContainer addSubview:self.label2];
+    [self.messageContainer addSubview:self.label3];
+    [self.messageContainer addSubview:self.vanImage];
+    [self.messageContainer addSubview:self.houseImage];
+    [self.view addSubview:self.messageContainer];
+//
 //    [self.buttonContainer addSubview:self.okButton];
 //    [self.buttonContainer addSubview:self.waitMoreButton];
 //    [self.view addSubview:self.buttonContainer];
-//
-//    [self setupInitialConstraints];
-//}
+//    
+//    
+    
 
-
-- (void) setupInitialConstraints {
-    
-//    NSDictionary *views = @{ @"label" : self.label,
-//                             @"okbutton" : self.okButton,
-//                             };ok
-
-    // label 1
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.f]];
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeTop multiplier:1.0f constant:20.f]];
-    
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeLeft multiplier:1.0f constant:20.f]];
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeRight multiplier:1.0f constant:-20.f]];
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:40.f]];
-    
-    // label 2
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label2 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.f]];
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label2 attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.label attribute:NSLayoutAttributeBottom multiplier:1.0f constant:20.f]];
-    
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label2 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeLeft multiplier:1.0f constant:20.f]];
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label2 attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.messageContainer attribute:NSLayoutAttributeRight multiplier:1.0f constant:-20.f]];
-    
-    [self.messageContainer addConstraint:[NSLayoutConstraint constraintWithItem:self.label2 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:200.f]];
-    
-    
-    
-    // button
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.okButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.f]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.okButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.label attribute:NSLayoutAttributeBottom multiplier:1.0f constant:20.f]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.okButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:30.f]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.okButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:200.f]];
-    
-    // Wait
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.waitMoreButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.f]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.waitMoreButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.okButton attribute:NSLayoutAttributeBottom multiplier:1.0f constant:20.f]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.waitMoreButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:30.f]];
-    
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.waitMoreButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:200.f]];
-    
-    
-    
 }
-- (void)viewDidLoad {
-    [super viewDidLoad];
 
+- (void)viewWillLayoutSubviews {
+    
+    [super viewWillLayoutSubviews];
+    CGRect frame = self.view.bounds;
+
+    frame.size.height = 522.0f;
+    self.messageContainer.frame = frame;
+    
+    
+    CGFloat labelWidth = frame.size.width - 40.0f;
+    
+    CGSize label1Size = [self.label sizeThatFits:CGSizeMake(labelWidth, 0.f)];
+    CGSize label2Size = [self.label2 sizeThatFits:CGSizeMake(labelWidth, 0.f)];
+    CGSize label3Size = [self.label3 sizeThatFits:CGSizeMake(labelWidth, 0.f)];
+   
+    self.label.frame = CGRectIntegral(CGRectMake(20.0f, 80.0f, label1Size.width, label1Size.height));
+    
+    self.label2.frame = CGRectIntegral(CGRectMake(20.0f, CGRectGetMaxY(self.label.frame) + 20.0f, labelWidth, label2Size.height));
+        
+    self.label3.frame = CGRectIntegral(CGRectMake(20.0f, CGRectGetMaxY(self.label2.frame) + 20.0f, labelWidth, label3Size.height));
+                                       
+    CGSize vanSize = CGSizeMake(75.f, 50.f);
+    CGSize houseSize = CGSizeMake(50.f, 35.f);
+    
+    self.vanImage.frame = CGRectIntegral(CGRectMake(20.f, CGRectGetMaxY(self.label3.frame) + 20.0f, vanSize.width, vanSize.height));
+    self.houseImage.frame = CGRectIntegral(CGRectMake(frame.size.width - houseSize.width - 30.f, CGRectGetMaxY(self.vanImage.frame) - houseSize.height, houseSize.width, houseSize.height));
+    
+    CGRect messageFrame = self.messageContainer.frame;
+    messageFrame.size.height = CGRectGetMaxY(self.vanImage.frame);
+    self.messageContainer.frame = messageFrame;
+    
 }
 
 - (void)didReceiveMemoryWarning {
