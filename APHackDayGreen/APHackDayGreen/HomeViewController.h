540ae44e1da85fd44c4527eb02908ef1ba6be43b
Maria La Porta <mlaporta@odecee.com.au>
2015-05-26 11:58:18 +1000

    center the message

---
+++ APHackDayGreen/APHackDayGreen/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  APHackDayGreen
+//
+//  Created by Maria La Porta on 26/05/2015.
+//  Copyright (c) 2015 ap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
